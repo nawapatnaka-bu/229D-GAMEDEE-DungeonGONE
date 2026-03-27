93c280195ea3829a7f2cccbef2ff1fd0b19ccdc3
nawapatnaka-bu <nawapat.naka@bumail.net>
2026-03-28 01:08:38 +0700

    Commit Add Sound and MainMenu Endcredit

---
+++ Assets/Script/MainMenuController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenuController : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    
+    public void QuitGame()
+    {
+        
+        Debug.Log("Quit Game!");
+
+       
+        Application.Quit();
+    }
+}
